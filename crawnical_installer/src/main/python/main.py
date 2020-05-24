25b79f29d98d8467bf64106cd8c6934a0dd23e4f
Charles Asselin <charles@asselin.xyz>
2020-05-24 11:40:46 -0400

    Working prototype on macOS
    Basic Lightroom file installer with little UI

---
+++ crawnical_installer/src/main/python/main.py
@@ -0,0 +1,65 @@
+from fbs_runtime.application_context.PySide2 import ApplicationContext
+from pathlib import Path
+from PySide2 import QtUiTools, QtCore, QtGui, QtXml 
+import sys
+import os
+import shutil
+import platform
+
+def main():
+    appctxt = ApplicationContext()
+    presets = []
+    for preset in Path(appctxt.get_resource("Presets")).iterdir():
+        presets.append(preset)
+
+    ui_path = appctxt.get_resource("installer_design.ui")
+    icon_path = appctxt.get_resource("icon_blue.png")
+    window = QtUiTools.QUiLoader().load(ui_path)
+    pixmap = QtGui.QPixmap(icon_path)
+    window.label.setPixmap(pixmap)
+    window.pushButton.clicked.connect(lambda: run(presets, window))
+    window.show()
+    exit_code = appctxt.app.exec_()
+    return exit_code
+    
+def run(presets, window):
+    home_folder = os.path.expanduser('~')
+    user_platform = platform.system()
+
+    if user_platform == "Darwin":
+        for preset in presets:
+            preset_path = Path(preset)
+            # Target path 1 is for previous versions of lightroom
+            target_path_1 = Path(home_folder) / "Library" / "Application Support" / "Adobe" / "Lightroom" / "Develop Presets" / "Crawnical Preset Pack"
+            target_path_1.mkdir(parents=True, exist_ok=True)
+            target_path_1 = target_path_1 / preset_path.name
+            # Target path 2 is for new versions of lightroom
+            target_path_2 = Path(home_folder) / "Library" / "Application Support" / "Adobe" / "CameraRaw" / "Settings" / "Crawnical Preset Pack"
+            target_path_2.mkdir(parents=True, exist_ok=True)
+            target_path_2 = target_path_2 / preset_path.name
+            shutil.copyfile(str(preset_path), str(target_path_1))
+            shutil.copyfile(str(preset_path), str(target_path_2))
+
+    if user_platform == "Windows":
+        for preset in presets:
+            preset_path = Path(preset)
+            # Target path 1 is for previous versions of lightroom
+            target_path_1 = "C:" / Path(home_folder) / "Downloads" / "AppData" / "Roaming" / "Adobe" / "Lightroom" / "Develop Presets" / "Crawnical Preset Pack"
+            target_path_1.mkdir(parents=True, exist_ok=True)
+            target_path_1 = target_path_1 / preset_path.name
+            # Target path 2 is for new versions of lightroom
+            target_path_2 = "C:" / Path(home_folder) / "Downloads" / "AppData" / "Roaming" / "Adobe" / "CameraRaw" / "Settings" / "Crawnical Preset Pack"
+            target_path_2.mkdir(parents=True, exist_ok=True)
+            target_path_2 = target_path_2 / preset_path.name
+
+            shutil.copyfile(str(preset_path), str(target_path_1))
+            shutil.copyfile(str(preset_path), str(target_path_2))
+
+    if user_platform == "Linux":
+        print("This application does not support Linux")    
+
+    window.progressBar.setValue(100)
+
+if __name__ == '__main__':
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
+    sys.exit(main())
---
+++ tests/test_crawnical_installer.py
@@ -0,0 +1,5 @@
+from crawnical_installer import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
